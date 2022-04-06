32f716531452f39ae1804282ed88bb1595d395f5
song <3224266014@qq.com>
2022-04-06 20:30:54 +0800

    fix: 修复自动更新流程

--- app/main/updater.ts
+++ app/main/updater.ts
@@ -1,47 +1,55 @@
+import { dialog } from 'electron';
 import { autoUpdater } from 'electron-updater';
-
-function sendStatusToWindow(text) {
-  mainWindow.webContents.send('message', text);
-}
 
 export default function checkVersionUpdate() {
   autoUpdater.autoDownload = false; // 将自动下载包设置为false
 
   //检测更新
-  // autoUpdater.checkForUpdates();
-  autoUpdater.checkForUpdatesAndNotify();
+  autoUpdater.checkForUpdates();
+  // autoUpdater.checkForUpdatesAndNotify();
 
   //监听'error'事件
   autoUpdater.on('error', (err) => {
-    sendStatusToWindow('Error in auto-updater. ' + err);
+    dialog.showErrorBox('Error: ', error == null ? 'unknown' : (error.stack || error).toString());
   });
 
   // 检查更新是否已开始时发出
-  autoUpdater.on('checking-for-update', () => {
-    sendStatusToWindow('Checking for update...');
-  });
+  autoUpdater.on('checking-for-update', () => {});
 
   // 检测有可更新的应用包
   autoUpdater.on('update-available', (info) => {
-    sendStatusToWindow('Update available.');
+    dialog
+      .showMessageBox({
+        type: 'info',
+        title: 'Found Updates',
+        message: 'Found updates, do you want update now?',
+        buttons: ['Sure', 'No'],
+      })
+      .then((buttonIndex) => {
+        if (buttonIndex === 0) {
+          autoUpdater.downloadUpdate();
+        }
+      });
   });
 
   // 检测没有可用更新时发出
   autoUpdater.on('update-not-available', (info) => {
-    sendStatusToWindow('Update not available.');
+    dialog.showMessageBox({
+      title: 'No Updates',
+      message: 'Current version is up-to-date.',
+    });
   });
 
   // 下载可更新的安装包
   autoUpdater.on('update-downloaded', (info) => {
-    sendStatusToWindow('Update downloaded');
-  });
-
-  // 监听下载进度
-  autoUpdater.on('download-progress', (progressObj) => {
-    let log_message = 'Download speed: ' + progressObj.bytesPerSecond;
-    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-    log_message = log_message + ' (' + progressObj.transferred + '/' + progressObj.total + ')';
-    sendStatusToWindow(log_message);
+    dialog
+      .showMessageBox({
+        title: 'Install Updates',
+        message: 'Updates downloaded, application will be quit for update...',
+      })
+      .then(() => {
+        setImmediate(() => autoUpdater.quitAndInstall());
+      });
   });
 }
 
